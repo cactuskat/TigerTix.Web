e7c522c64bccfca03f8917084acffd35f715915f
10blal <demarco.blagburn@gmail.com>
2024-04-02 17:54:43 -0400

    Added eventcontroller file; keep in mind it is unfinished but would appreciate some help with it in case there is something I'm doing wrong

---
+++ Controllers/EventController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TigerTix.Web.Models;
+
+namespace TigerTix.Web.Controllers
+{
+    public class EventController : Controller
+    {
+        public IActionResult Index() // Return Values are placeholders; this is unfinished
+        {
+            return View();
+        }
+
+        [HttpGet("/Events")]
+        public IActionResult GetEvents()
+        {
+            return View();
+        }
+
+        [HttpPost("/Events/:EventId/tags")]
+        public IActionResult RetrieveTags()
+        {
+            return View();
+        }
+
+        [HttpPost("/Events")]
+        public IActionResult PostEvent()
+        {
+            return View();
+        }
+    }
+}
