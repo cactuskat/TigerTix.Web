da9b6b4a1b1bae0e0634cf2fe1a5c7b9918e9c63
Karina <k.annabel0@gmail.com>
2024-04-07 21:50:43 -0400

    Add sign up and login with cuid or email

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -8,19 +8,20 @@ namespace TigerTix.Web.Controllers
 {
     public class HomeController : Controller
     {
-        private static List<Event> events = new List<Event>();
+        private List<Event> events;
         private static List<Event> cart = new List<Event>();
+
+        public HomeController()
+        {
+            events = new List<Event>
+            {
+                new Event { Id = 1, Name = "Soccer Game", Date = new DateTime(2024, 5, 3), Price = 30 },
+                new Event { Id = 2, Name = "Basketball Game", Date = new DateTime(2024, 4, 10), Price = 20 }
+            };
+        }
+
         public IActionResult Homepage()
         {
-            events.Add(new Event { Id = 1,Name = "Soccer Game", Date = new DateTime(2024, 5, 3), Price = 30 });
-            events.Add(new Event { Id = 2,Name = "Basketball Game", Date = new DateTime(2024, 4, 10), Price = 20 });
-            /*
-            var events = new List<Event>
-            {
-            new Event { Name = "Soccer Game", Date = new DateTime(2024, 5, 3), Price = 30 },
-            new Event { Name = "Basketball Game", Date = new DateTime(2024, 4, 10), Price = 20 }
-            };
-            */
             return View(events);
         }
 
@@ -34,7 +35,7 @@ namespace TigerTix.Web.Controllers
         public IActionResult AddToCart(int eventId)
         {
             // Add the event to the cart (you can implement this logic)
-            var selectedEvent = events.FirstOrDefault(e => e.Id == eventId);
+            var selectedEvent = events.Find(e => e.Id == eventId);
             if (selectedEvent != null && !selectedEvent.inCart)
             {
                 selectedEvent.inCart = true;
@@ -45,6 +46,16 @@ namespace TigerTix.Web.Controllers
 
         public IActionResult ViewCart() {
             return View(cart);
+        }
+
+        public IActionResult RemoveFromCart(int eventId) {
+            var selectedEvent = events.Find(e => e.Id == eventId);
+            if (selectedEvent != null)
+            {
+                selectedEvent.inCart = false;
+                cart.Remove(selectedEvent);
+            }
+            return RedirectToAction("ViewCart");
         }
 
     }
